846919f60b422609fa23c2ecfedb0d85123644da
chocostiko <ejangcanan07@gmail.com>
2026-07-20 13:58:20 +0800

    fix: add 'x-user-id' to allowed headers for tenant request processing

--- backend/api/process_tenant_request.php
+++ backend/api/process_tenant_request.php
@@ -2,7 +2,7 @@
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Admin-Id");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Admin-Id, x-user-id");
 
 require_once "../config.php";
 
